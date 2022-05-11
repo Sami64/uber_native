3531851594c1f3ca72498e2e8bc7c465aaa6df86
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-05-11 22:26:09 +0000

    ⚡️ riders

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useRef, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
 import { selectOrigin, selectDestination } from "../slices/navSlice.js";
@@ -10,9 +10,19 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 const Map = () => {
 	const origin = useSelector(selectOrigin);
 	const destination = useSelector(selectDestination);
+	const mapRef = useRef(null);
+
+	useEffect(() => {
+		if (!origin || !destination) return;
+
+		mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+			edgePadding: { top: 50, right: 50, left: 50, bottom: 50 },
+		});
+	}, [origin, destination]);
 
 	return (
 		<MapView
+			ref={mapRef}
 			style={tw`flex-1`}
 			mapType="mutedStandard"
 			initialRegion={{
@@ -40,6 +50,17 @@ const Map = () => {
 					title="Origin"
 					description={origin.description}
 					identifier="origin"
+				/>
+			)}
+			{destination?.location && (
+				<Marker
+					coordinate={{
+						latitude: destination.location.lat,
+						longitude: destination.location.lng,
+					}}
+					title="Destination"
+					description={destination.description}
+					identifier="destination"
 				/>
 			)}
 		</MapView>
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TouchableOpacity,
+} from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+	{ id: "", image: "https://links.papareact.com/3pn" },
+	{ id: "", image: "https://links.papareact.com/5w8" },
+	,
+	{ id: "", image: "https://links.papareact.com/7pf" },
+];
 
 const RideOptionsCard = () => {
-  return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
-  )
-}
+	const navigation = useNavigation();
+	return (
+		<SafeAreaView style={tw`bg-white flex-grow`}>
+			<View>
+				<TouchableOpacity
+					onPress={() => navigation.navigate("NavigateCard")}
+					style={tw`absolute top-3 z-50 left-5 p-3`}
+				>
+					<Icon type="fontawesome" name="chevron-left" />
+				</TouchableOpacity>
+				<Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+			</View>
+		</SafeAreaView>
+	);
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
